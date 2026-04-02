677478ec5431cde666d905cf09f8dbc66f9603cc
dedisemara <semaradedi@gmail.com>
2026-04-02 11:05:20 +0800

    08-prime-numbers V2

--- 08-prime-numbers/1.js
+++ 08-prime-numbers/1.js
@@ -1,30 +1,35 @@
 const readlineSync = require('readline-sync');
 
-const total = +readlineSync.question('Enter the number of prime numbers: ');
-console.log(`The first ${total} prime number are`);
+const inputPrime = readlineSync.question('Enter the number of prime number: ');
+console.log(`The first ${inputPrime} prime number are`);
 
-// prime number --> only has exactly two factors: 1 and the number itself.
-let count = 0;
-let number = 2; //starting primary number
+let countNumber = 0;
+let startPrimerNumber = 2;
 
-while (count < total) {
-    let divisorCount = 0;
-    let i = 2;
+while(countNumber < inputPrime) { //when count number who printed < input
+    //  why i and devisorForPrime devine on the while loop. because it will restart to starting value (i = 2) && (devisorForPrime = 0)
+    let i = 2; // should be same as startPrimeNumber
+    let devisorForPrime = 0; //the right result devisor for prime number
+    console.log('check divisor : ' + devisorForPrime);
+    console.log('check i : ' + i);
 
-    while (i < number) {
-        if (number % i === 0) {
-            divisorCount++;
-            // console.log(`nominal devisor: ${divisorCount}`)
+
+    while(i < startPrimerNumber){ // this while do to test divisor for prime
+        if(startPrimerNumber % i === 0){
+            devisorForPrime++
+            console.log('divisor on the looping : ' + devisorForPrime)
         }
         i++;
-        // console.log(`nominal I : ${i}`)
+        console.log('check i on the looping: ' + i);
+        
     }
 
-    if (divisorCount === 0) {
-        console.log(number);
-        count++;
+    if(devisorForPrime === 0) { //this conditional make decission if this is prime number dan i === start prime number
+        console.log(startPrimerNumber);
+        countNumber++;
     }
+    startPrimerNumber++;
+    console.log('check value : ' + startPrimerNumber);
 
-    number++;
-    // console.log(`nominal number: ${number}`)
 }
+
