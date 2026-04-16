74ff30b965a1c3961533816135eb01a331a321d3
dedisemara <semaradedi@gmail.com>
2026-04-16 12:17:40 +0800

    revise

--- 12-find-largest-element/index.js
+++ 12-find-largest-element/index.js
@@ -2,32 +2,55 @@ const readlineSync = require('readline-sync');
 const indexTable = readlineSync.question('Enter the number of rows and columns in the array: ');
 const indexArray = indexTable.split(' ')
 const row = +indexArray[0];
+const col = +indexArray[1];
+// console.log(col);
 
 const arrayTable = []
 for(let i = 0; i < row; i++){
     const colom = readlineSync.question('Input value: ')
-    arrayTable.push(colom.split(' '));
-}
-// console.log(arrayTable);
+    let arrDataColom = [];
+    const dataColom = colom.split(' ');
 
+    for(let n = 0; n < dataColom.length; n++){
+        if (n < col){
+            arrDataColom.push(dataColom[n]);
+        }
+    }
+    arrayTable.push(arrDataColom);
+}
+
+// dami
 // const arrayTable = [
 //     [ '7', '8', '9', '1' ], 
-//     [ '2', '3', '7.3', '1' ], 
-//     [ '2', '5', '12.3', '15' ],
+//     [ '2', '3', '7.3', '100' ], 
+//     [ '2', '5', '12.3', '20' ],
 // ];
 
-// check max value on inside row
+// check max value inside row 
 const maxDimension = [];
-for (let i = 0; i < arrayTable.length; i++){
-    const max = Math.max(...arrayTable[i]);
-    maxDimension.push(+max);
-};
+for(let i = 0; i < arrayTable.length; i++){
+    let expMax = arrayTable[i][0];
+    for(let n = 1; n < arrayTable[i].length; n++){
+        const vallueArray = +arrayTable[i][n];
+        if (expMax < vallueArray){
+            expMax = vallueArray;
+        }
+    }
+    // console.log(`value expMax ${expMax}`);
+    maxDimension.push(expMax);
+}
 
-// console.log(maxDimension);
+// check overall value from maxDimension
+let maxValue = +maxDimension[0];
+for(let i = 1; i < maxDimension.length; i++){
+    if(maxValue < maxDimension[i]){
+        maxValue = maxDimension[i];
+    }
+}
 for(let i = 0; i < arrayTable.length; i++){
     for(let n = 0; n < arrayTable[i].length; n++){
-        if(+arrayTable[i][n] === Math.max(...maxDimension)){
-            console.log(`row ${i} and row ${n}`);
+        if(+arrayTable[i][n] === maxValue){
+            console.log(`The location of the largest element is ${maxValue} at (${i}, ${n})`);
         }
     }
 };
